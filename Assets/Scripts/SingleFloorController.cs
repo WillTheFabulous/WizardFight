6afa51c4e32dea00f9acfccbfadd756ad947863e
hehehaha12139 <zelda2013e3@gmail.com>
2021-04-27 23:02:39 -0400

    Add color change on floors

--- Assets/Scripts/SingleFloorController.cs
+++ Assets/Scripts/SingleFloorController.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class SingleFloorController : MonoBehaviour
 {
     public bool isFallen = false;
+    private Material redMat;
+    private Material curMat;
+    private int preCount = 50;
+    private int cnt = 0;
+    private Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = this.GetComponent<Transform>().GetChild(1).GetComponent<Renderer>();
+        curMat = rend.material;
+        redMat = new Material(curMat);
+        //redMat.CopyPropertiesFromMaterial(curMat);
+        redMat.color = new Color(1.0f, 0.0f, 0.0f);
     }
 
     // Update is called once per frame
@@ -21,7 +30,18 @@ public class SingleFloorController : MonoBehaviour
     {
         if (isFallen) 
         {
-            this.GetComponent<Transform>().position -= new Vector3(0.0f, 1.0f, 0.0f);
+            if (cnt < preCount)
+            {
+                float interp = (float)(cnt % 10) / 10.0f;
+                rend.material.Lerp(curMat, redMat, interp);
+                cnt++;
+            }
+            else 
+            {
+                this.GetComponent<Transform>().position -= new Vector3(0.0f, 1.0f, 0.0f);
+            }
+            
+           
             //if(this.GetComponent<Transform>().position.y <= -100.0f)
                 //Destroy(gameObject);
         }
